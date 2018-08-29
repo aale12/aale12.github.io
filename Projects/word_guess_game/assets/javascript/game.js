1354f5ed680dae57b92c981ec650638f4263362a
aale12 <leanthony104@yahoo.com>
2018-08-28 23:02:24 -0500

    word guess game final

--- Projects/word_guess_game/assets/javascript/game.js
+++ Projects/word_guess_game/assets/javascript/game.js
@@ -43,8 +43,8 @@ function resetGame() //resets the game when you win or lose
     emptyWordVisible.textContent = currentGuess;
     guessesLeftVisible.textContent = guessesLeft + " guesses left.";
     currentGuess = "";
-    console.log(letters);
-    console.log(theWord);
+    //console.log(letters);
+    //console.log(theWord);
 }
 function gameStatus() { //checks if youve won or lost
     if (guessesLeft >= 0 && lettersRemain === 0) {
@@ -55,24 +55,24 @@ function gameStatus() { //checks if youve won or lost
 }
 
 function gameVictory() {
-        gameStarted = false;
-        emptyWordVisible.classList.add("green");
+        gameStarted = false; //if the game is stopped they cant add anymore input
+        emptyWordVisible.classList.add("green"); //color the correct word green
         winCounter++;
         document.getElementById("won").textContent = "Won: " + winCounter;
-        guessedLettersVisible.textContent = "You have guessed correctly!" + guessedLetters;
+        guessedLettersVisible.textContent = "You have guessed correctly!"
         guessedLetters = [];
-        // resetGame();
+        document.getElementById("victoryTune").play(); //plays victory music
 }
 
 function gameLost() {
-        gameStarted = false;
-        emptyWordVisible.classList.add("red");
+        gameStarted = false; 
+        emptyWordVisible.classList.add("red"); //show the answer and color it red to show their failure
         emptyWordVisible.textContent = theWord;
         loseCounter++;
         document.getElementById("lost").textContent = "Lost: " + loseCounter;
         guessedLettersVisible.textContent = "You have guessed incorrectly: " + guessedLetters;
         guessedLetters = [];
-        // resetGame();
+        document.getElementById("lossTune").play(); //plays lose music
 }
 
 window.onload = function(){
@@ -87,43 +87,42 @@ document.onkeyup = function (event)
 guess = event.key.toLowerCase();
     if(gameStarted)
     {
-    if(event.keyCode >= 65 && event.keyCode <= 90)// can only type a-z
-    {
-    if (letters.indexOf(guess) > -1) // check if guess is in the answer
-    { 
-        if (correctLetters.indexOf(guess) === -1) //check if the guess is already guessed
-        {  
-            correctLetters.push(guess); //add the guessed correct letter to an invisible correct letters array so it cant be picked again
-            for (var i = 0; i < theWord.length; i++)
-            {
-                if (letters[i] === guess) // if the guess is in this spot
-                {
-                    onScreen[i] = guess; //put that there on the array that will go in the screen and reduce the progress to win by 1
-                    lettersRemain--;
-                    console.log("lettersRemain="+lettersRemain);
-                }
-                currentGuess = currentGuess + onScreen[i] + " "; //add the correct answer to the screen with a space
-                emptyWordVisible.textContent = currentGuess;// show it in the html
-            }
-            currentGuess = ""; // reset the letter to be added
-        }
-    }else   //if its not in the word
+        if(event.keyCode >= 65 && event.keyCode <= 90)// can only type a-z
         {
-        if (guessedLetters.indexOf(guess) < 0) // if it hasnt been guessed before 
-            {
-                guessedLetters.push(guess); //add that guess to the array of your (wrong) guesses
-                guessedLettersVisible.textContent = "You have guessed: " + guessedLetters; //show what youve guessed (wrongly) and take away a guess
-                guessesLeft--;
-                console.log("guessesLeft="+guessesLeft);
-                guessesLeftVisible.textContent = guessesLeft + " guesses left!"; //tell you how many guesses you have left
-            }
+            if (letters.indexOf(guess) > -1) // check if guess is in the answer
+            { 
+                if (correctLetters.indexOf(guess) === -1) //check if the guess is already guessed
+                {  
+                    correctLetters.push(guess); //add the guessed correct letter to an invisible correct letters array so it cant be picked again
+                    for (var i = 0; i < theWord.length; i++)
+                    {
+                        if (letters[i] === guess) // if the guess is in this spot
+                        {
+                            onScreen[i] = guess; //put that there on the array that will go in the screen and reduce the progress to win by 1
+                            lettersRemain--;
+                            //console.log("lettersRemain="+lettersRemain);
+                        }
+                        currentGuess = currentGuess + onScreen[i] + " "; //add the correct answer to the screen with a space
+                        emptyWordVisible.textContent = currentGuess;// show it in the html
+                    }
+                    currentGuess = ""; // reset the letter to be added
+                }
+            }else   //if its not in the word
+                {
+                if (guessedLetters.indexOf(guess) < 0) // if it hasnt been guessed before 
+                    {
+                        guessedLetters.push(guess); //add that guess to the array of your (wrong) guesses
+                        guessedLettersVisible.textContent = "You have guessed: " + guessedLetters; //show what youve guessed (wrongly) and take away a guess
+                        guessesLeft--;
+                        //console.log("guessesLeft="+guessesLeft);
+                        guessesLeftVisible.textContent = guessesLeft + " guesses left!"; //tell you how many guesses you have left
+                    }
+                }
+            gameStatus();
         }
-        gameStatus();
-    }
     else
-    {
-        alert("Please enter a letter");
-        // resetGame();
+        {
+            alert("Please enter a letter");
+        }
     }
-}
 }
