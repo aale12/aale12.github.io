1440ccde97cd7f358aa472bad6eba24240dd31b7
aale12 <leanthony104@yahoo.com>
2018-09-10 20:30:45 -0500

    star wars comment

--- Projects/star-wars-rgp/assets/javascript/game.js
+++ Projects/star-wars-rgp/assets/javascript/game.js
@@ -1,3 +1,4 @@
+//object of objects of heros
 var heroSelection =
 {
     "1":
@@ -22,6 +23,7 @@ var heroSelection =
     }
 }
 
+//object of objects of enemies
 var enemies =
 {
     "0":
@@ -45,28 +47,30 @@ var enemies =
         health: 200, attackPower: null, counterPower: 25, image: "./assets/media/images/freedonImage.jpg"
     }
 }
-
+//Chosen Character object and Health/Attack variables
 var myChar = {};
 var myAttack;
 var myLife;
 
+//Chosen Enemy Object and Health/Attack Variables
 var enemy = {};
 var enemyAttack;
 var enemyLife;
 
-var identifier;
+var identifier; //identifier for which character/enemy was clicked to store them
+
 var characterChosen = false;
 var enemyChosen = false;
 var gameEnded = false;
 
 var victories = 0;
 
-var enemyID;
-//console.log(heroSelection[0].name);
+//Audio Files
 var bgm = new Audio('./assets/media/audio/The Occupation of Balmorra.mp3');
 var blaster = new Audio('./assets/media/audio/blasterFire.mp3');
 var clash = new Audio('./assets/media/audio/lightsaberClash.mp3');
 
+//modifies the myChar object to match the chosen hero
 function storeChar() {
     for (i = 0; i < 4; i++) {
         switch (identifier) {
@@ -80,6 +84,8 @@ function storeChar() {
         myLife = myChar.health;
     }
 }
+
+//modifies enemy object to match chosen enemy
 function storeEnemy() {
     for (i = 0; i < 4; i++) {
         switch (identifier) {
@@ -95,22 +101,27 @@ function storeEnemy() {
     }
 }
 
+//replicates chosen character to then attacker zone
 function renderChar() {
     $("#charSelected").append($("<img class = 'img-fluid'>").attr("src", myChar.image))
         .append($("<p>").text(myChar.name))
         .append($("<p class = 'hp'>").text(myChar.health + "HP"));
 }
 
+//replicates chosen enemy to defender zone
 function renderEnemy() {
     $("#enemySelected").append($("<img class = 'img-fluid'>").attr("src", enemy.image))
         .append($("<p>").text(enemy.name))
         .append($("<p class = 'hp'>").text(enemy.health + "HP"));
 }
 
+//makes remaining enemies visible for battle selection
 function reselectEnemy() {
     clearDefeatedEnemy();
     $(".enemy, enemiesText, #ajuntaDoc, #nagaDoc, #freedonDoc, #markaDoc").removeClass("hidden");
 }
+
+//removes the recently defeated enemy from list of available enemies to defeat
 function clearDefeatedEnemy() {
     switch (identifier.toString()) {
         case "0": $("#ajuntaEnemy, #ajuntaDoc").addClass("invisible");
@@ -124,6 +135,7 @@ function clearDefeatedEnemy() {
     }
 }
 
+//resets the variables for a new game
 function renderGame() {
     gameEnded = false;
     characterChosen = false;
@@ -131,18 +143,16 @@ function renderGame() {
     victories = 0;
     myChar = {};
     enemy = {};
-    $("#enemySelected").empty();
-    $("#charSelected").empty();
-    $("#myChar").addClass("hidden");
-    $("#defenderDoc").addClass("hidden");
-    $(".availableChar, #reyDoc, #lukeDoc, #finnDoc, #hanDoc").removeClass("hidden");
-    $(".enemy, #ajuntaDoc, #nagaDoc, #freedonDoc, #markaDoc").removeClass("hidden invisible");
+    $("#enemySelected, #charSelected").empty();
+    $("#myChar, #defenderDoc").addClass("hidden");
+    $(".availableChar, #reyDoc, #lukeDoc, #finnDoc, #hanDoc, .enemy, #ajuntaDoc, #nagaDoc, #freedonDoc, #markaDoc").removeClass("hidden invisible");
     $(".restartButton").addClass("hidden");
     $("#gameMessage").text("Choose your character!");
 }
 
 alert("Forgotten Dark Lords of the Sith from a retconned universe have invaded! Select your hero to drive them back!");
 
+//audio button
 $(".audioButton").on("click", function () {
     if (bgm.paused == false) {
         bgm.pause();
@@ -151,10 +161,15 @@ $(".audioButton").on("click", function () {
         bgm.play();
         $(".audioButton").text("Audio Off")
     }
-})
+});
+
+
+//reset button
 $(".restartButton").on("click", function () {
     renderGame();
 });
+
+//stores and renders chosen character, and hides unchosen characters.
 $(".availableChar").on("click", function () {
     if (characterChosen === false) {
         identifier = $(this).attr("value");
@@ -167,6 +182,7 @@ $(".availableChar").on("click", function () {
     }
 });
 
+//stores and renders enemy, hides other enemies, and preps the battle area
 $(".enemy").on("click", function () {
     if (characterChosen === true && enemyChosen === false) {
         identifier = $(this).attr("value");
@@ -180,46 +196,49 @@ $(".enemy").on("click", function () {
     }
 });
 
+//removes defeated enemy from defender, puts remaining enemies back except those that were defeated
 $(".continueButton").on("click", function () {
     $("#enemySelected").empty();
     reselectEnemy();
     $(".continueButton").addClass("hidden");
     $("#gameMessage").text("Choose your next opponent");
 })
-$(".attackButton").on("click", function () {
-    if (gameEnded === false) {
-        $("#gameMessage").empty();
-        enemyLife -= myAttack;
-        $("#enemySelected").children(".hp").text(enemyLife + " HP remaining");
 
-        if (enemyLife > 0) {
-            $("#gameMessage").text("You attack " + enemy.name + " for " + myAttack + " damage!");
-            myAttack += myChar.attackPower;
-            myLife -= enemy.counterPower;
-            clash.play();
-            if (myLife > 0) {
-                $("#charSelected").children(".hp").text(myLife + " HP remaining");
-                $("#gameMessage").append("<br />" + enemy.name + " Attacks you for " + enemyAttack + " damage!");
-            } else {
-                gameEnded = true;
+//attack function
+$(".attackButton").on("click", function () {
+    if (gameEnded === false) {  //if you havent won or lost
+        $("#gameMessage").empty();  // clear the message
+        enemyLife -= myAttack;  //deal damage to enemy
+        $("#enemySelected").children(".hp").text(enemyLife + " HP remaining"); //update enemy HP value on the page
+
+        if (enemyLife > 0) {  //if enemy is still alive
+            $("#gameMessage").text("You attack " + enemy.name + " for " + myAttack + " damage!"); //tell user how much damage they dealt
+            myAttack += myChar.attackPower; //increaes player attack power by the base amount
+            myLife -= enemy.counterPower; //enemy deals damage to player
+            clash.play(); //play battle sound effect
+            if (myLife > 0) { //if you are still alive
+                $("#charSelected").children(".hp").text(myLife + " HP remaining"); //update player health on the page
+                $("#gameMessage").append("<br />" + enemy.name + " Attacks you for " + enemyAttack + " damage!"); //on a new line, tell user how much damage was dealt to them
+            } else { // if you are dead
+                gameEnded = true; //game is over, attack button is removed and restart button added, adds relevant messages
                 $("#charSelected").children(".hp").text("RIP");
                 $("#gameMessage").text("You were defeated!");
                 $(".attackButton").addClass("hidden");
                 $(".restartButton").removeClass("hidden");
             }
-        } else {
-            victories++;
+        } else { //if enemy is dead
+            victories++; //add 1 to victory count
             enemyChosen = false;
-            $(".attackButton").addClass("hidden");
-            if (victories < 4) {
-                $(".continueButton").removeClass("hidden");
+            $(".attackButton").addClass("hidden"); //remove attack button
+            if (victories < 4) { //if there are enemies remaining
+                $(".continueButton").removeClass("hidden"); // show the reselect enemy button
             }
-            $("#gameMessage").text("You have defeated " + enemy.name + "!");
-            $("#enemySelected").children(".hp").text("RIP");
-            if (victories === 4) {
+            $("#gameMessage").text("You have defeated " + enemy.name + "!"); //tell user who they defeated
+            $("#enemySelected").children(".hp").text("RIP"); //HP text to RIP
+            if (victories === 4) { //if all enemies are defeated
                 gameEnded = true;
-                $("#gameMessage").text("Congratulations! You are victorious!")
-                $(".restartButton").removeClass("hidden");
+                $("#gameMessage").text("Congratulations! You are victorious!") //you win!
+                $(".restartButton").removeClass("hidden"); //restart!
             }
         }
     }
