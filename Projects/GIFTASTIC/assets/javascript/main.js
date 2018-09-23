fc6cbd65c6b7ea3ad4a0eb8e681e06b7156e2b90
aale12 <leanthony104@yahoo.com>
2018-09-23 18:50:29 -0500

    modal and clipboard

--- Projects/GIFTASTIC/assets/javascript/main.js
+++ Projects/GIFTASTIC/assets/javascript/main.js
@@ -16,6 +16,7 @@ function renderButtons() {
       topics.push(input);
     }
     renderButtons();
+    $("#gif-search").val("");
   });
   renderButtons();
 
@@ -24,15 +25,19 @@ $(document).on("click", ".gif-btn", function () {
     gifID = $(this).val();
     //console.log(gif);
     queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=" + topics[gifID] + "&limit=10&offset=0&rating=PG-13&lang=en";
-
+    console.log(queryURL);
     $.ajax({ url: queryURL, method: "GET" }).then(function (res) {
         console.log(res);
         for (i = 0; i < res.data.length; i++) {
-            $("#gif-view").append("<figure ='rating'><figcaption class='text-center'> Rating: " + res.data[i].rating + "</figcaption><img class='m-2 gif' value='" + i + "' src='" + res.data[i].images.downsized_still.url + "'></figure>");
+            // $("#gif-view").append("<figure ='rating'><figcaption class='text-center'> Rating: " + res.data[i].rating + "</figcaption><img class='m-2 gif' value='" + i + "' src='" + res.data[i].images.downsized_still.url + "'></figure>");
+            $("#gif-view").append("<figure ='rating'><figcaption class='text-center'><button type='button' value=" + i + " id='clip-" + i + "' class='clip-btn btn btn-secondary'> Rating: " + res.data[i].rating + "</button></figcaption><img id='img-" + i + "' class='m-2 gif' value='" + i + "' src='" + res.data[i].images.downsized_still.url + "'></figure>");
         }
     });
 });
 
+function copyToClipboard(element) {
+    
+}
 $(document).on("click", ".gif", function () {
     var gifLink = $(this).attr("src");
     if ($(this).hasClass("play")) {
---
+++ Projects/GIFTASTIC/readme.md
@@ -0,0 +1,10 @@
+# SUPERGIFS - GIF Searcher
+## Overview
+This is a Gif searcher! It searches the Giphy API for 10 of the top related gifs!
+### Instructions
+* Click any of the buttons to search up that gif.
+* You can add your own gif topics by typing in the search box!
+* Click the gif to play it, click it again to pause.
+* There are 8 questions total.. Good Luck!
+
+
