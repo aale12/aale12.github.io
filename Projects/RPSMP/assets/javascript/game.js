34af5f1e9cd03e2c1734a99abeed0ea36a05900f
aale12 <leanthony104@yahoo.com>
2018-09-26 00:12:46 -0500

    min length chat

--- Projects/RPSMP/assets/javascript/game.js
+++ Projects/RPSMP/assets/javascript/game.js
@@ -6,31 +6,35 @@ var config = {
     projectId: "rpsmp-bea5f",
     storageBucket: "",
     messagingSenderId: "484874273337"
-  };
-  firebase.initializeApp(config);
-  var database = firebase.database();
-  var yourName = "Guest";
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+var yourName = "Guest";
 
-  $("#username-submit").on("click", function(event){
+$("#username-submit").on("click", function (event) {
     event.preventDefault();
     yourName = $("#username-input").val().trim();
-    var joinMessage = yourName + " has joined!";
-    console.log(joinMessage);
-    var chatKey = database.ref("/chat").push().key;
-    database.ref("chat/" + chatKey).set(joinMessage);
-	$("#username-input").val("");	
-  });
+    if (yourName.length > 0) {
+        var joinMessage = yourName + " has joined!";
+        console.log(joinMessage);
+        var chatKey = database.ref("/chat").push().key;
+        database.ref("chat/" + chatKey).set(joinMessage);
+    }
+    $("#username-input").val("");
+});
 
-  $("#chat-submit").on("click", function(event){
+$("#chat-submit").on("click", function (event) {
     event.preventDefault();
     var chatMessage = yourName + ": " + $("#chat-input").val().trim();
-    console.log(chatMessage);
+    if (chatMessage.length > 0) {
+        console.log(chatMessage);
+        var chatKey = database.ref("/chat").push().key;
+        database.ref("chat/" + chatKey).set(chatMessage);
+    }
     $("#chat-input").val("");
-    var chatKey = database.ref("/chat").push().key;
-    database.ref("chat/" + chatKey).set(chatMessage);
-  });
+});
 
-  database.ref("/chat").on("child_added", function(snap){
-      var message = snap.val();
-      $("#chatLog").append(message+"<br>");
-  })
+database.ref("/chat").on("child_added", function (snap) {
+    var message = snap.val();
+    $("#chatLog").append(message + "<br>");
+})
