32da2e1588000aba59de10d8bf6151144bd89bc6
aale12 <leanthony104@yahoo.com>
2018-09-29 02:35:34 -0500

    winlose logic

--- Projects/RPSMP/assets/javascript/game.js
+++ Projects/RPSMP/assets/javascript/game.js
@@ -40,25 +40,26 @@ connectionsRef.on("value", function (snap) {
     //$("#watchers").text(snap.numChildren());
 });
 
-function updatePlayerDisplay(player, playerData) {
-    $("#name-" + player).html(playerData)
+function updatePlayerDisplay(player, playerName,){// playerWins, playerLosses, playerTies) {
+    $("#name-" + player).html(playerName);
+    //$("stats-" + player).append("<li>Wins: " + playerWins + "</li><li>Losses: " + playerLosses + "</li><li>Ties: " + playerTies + "</li>");
 }
 fP.on("value", function (s) {
     var fP1Choice = s.child("p1/choice").val();
     var fP2Choice = s.child("p2/choice").val();
     if (s.child("p1").exists() === true) {
-        updatePlayerDisplay("player1", s.child("p1/name").val());
+        updatePlayerDisplay("player1", s.child("p1/name").val());//, s.child("p1/wins").val(), s.child("p1/losses").val(), s.child("p1/ties").val());
         if (refChoices.includes(fP1Choice)) {
             $("#p1-board").html("<img class = 'p-2 img-fluid' src='./assets/media/images/choicePend.png'>");
-            console.log(fP1Choice);
+            //console.log(fP1Choice);
         }
     } else {
         updatePlayerDisplay("player1", "Waiting for Player 1...");
         console.log("p1 does not exist");
     }
     if (s.child("p2").exists() === true) {
-        updatePlayerDisplay("player2", s.child("p2/name").val());
-        console.log("p2 exists");
+        updatePlayerDisplay("player2", s.child("p2/name").val());//, s.child("p2/wins").val(), s.child("p2/losses").val(), s.child("p2/ties").val());
+        //console.log("p2 exists");
         if (refChoices.includes(fP2Choice)) {
             $("#p2-board").html("<img class = 'p-2 img-fluid' src='./assets/media/images/choicePend.png'>");
             console.log(fP2Choice);
@@ -68,63 +69,134 @@ fP.on("value", function (s) {
         console.log("p2 does not exist");
     }
     if (s.child("p1").exists() && s.child("p2").exists() && refChoices.includes(fP1Choice) && refChoices.includes(fP2Choice)) {
-        switch (fP1Choice) {
-            case "rock":
-                switch (fP2Choice) {
-                    case "rock":
-                        console.log("draw");
-                        break;
-                    case "paper":
-                        console.log("p1lose");
-                        break;
-                    case "scissors":
-                        console.log("p1win");
-                        gameText.html(s.child("p1/name").val() + " wins!");
-                        break;
-                }
-                break;
-            case 'paper':
-                switch (fP2Choice) {
-                    case 'rock':
-                        console.log("p1win");
-                        break;
-                    case 'paper':
-                        console.log("draw");
-                        break;
-                    case 'scissors':
-                        console.log("p1lose");
-                        break;
-                }
-                break;
-            case 'scissors':
-                switch (fP2Choice) {
-                    case 'rock':
-                        console.log("p1lose");
-                        break;
-                    case 'paper':
-                        console.log("p1win");
-                        break;
-                    case 'scissors':
-                        console.log("draw");
-                        break;
-                }
-                break;
+        // switch (fP1Choice) {
+        //     case "rock":
+        //         switch (fP2Choice) {
+        //             case "rock":
+        //                 p1.ties++;
+        //                 p2.ties++;
+        //                 console.log("draw");
+        //                 gameText.html("It's a draw!");
+        //                 break;
+        //             case "paper":
+        //                 p1.losses++;
+        //                 p2.wins++;
+        //                 console.log("p1lose");
+        //                 gameText.html(s.child("p2/name").val() + " wins!");
+        //                 break;
+        //             case "scissors":
+        //                 p1.wins++;
+        //                 p2.losses++;
+        //                 console.log("p1win");
+        //                 gameText.html(s.child("p1/name").val() + " wins!");
+        //                 break;
+        //         }
+        //         break;
+        //     case 'paper':
+        //         switch (fP2Choice) {
+        //             case 'rock':
+        //                 p1.wins++;
+        //                 p2.losses++;
+        //                 console.log("p1win");
+        //                 gameText.html(s.child("p1/name").val() + " wins!");
+        //                 break;
+        //             case 'paper':
+        //                 p1.ties++;
+        //                 p2.ties++;
+        //                 console.log("draw");
+        //                 gameText.html("It's a draw!");
+        //                 break;
+        //             case 'scissors':
+        //                 p1.losses++;
+        //                 p2.wins++;
+        //                 console.log("p1lose");
+        //                 gameText.html(s.child("p2/name").val() + " wins!");
+
+        //                 break;
+        //         }
+        //         break;
+        //     case 'scissors':
+        //         switch (fP2Choice) {
+        //             case 'rock':
+        //                 p1.losses++;
+        //                 p2.wins++;
+        //                 console.log("p1lose");
+        //                 gameText.html(s.child("p2/name").val() + " wins!");
+
+        //                 break;
+        //             case 'paper':
+        //                 p1.wins++;
+        //                 p2.losses++;
+        //                 console.log("p1win");
+        //                 gameText.html(s.child("p1/name").val() + " wins!");
+
+        //                 break;
+        //             case 'scissors':
+        //                 p1.ties++;
+        //                 p2.ties++;
+        //                 console.log("draw");
+        //                 gameText.html("It's a draw!");
+
+        //                 break;
+        //         }
+        //         break;
+        //}
+        if (fP1Choice === fP2Choice) {
+            p1.ties++;
+            p2.ties++;
+            console.log("draw");
+            gameText.html("It's a draw!");
+        } else if (fP1Choice === "rock") {
+            if (fP2Choice === "scissors") {
+                p1.wins++;
+                p2.losses++;
+                console.log("p1wins");
+                gameText.html(s.child("p1/name").val() + "wins!");
+            } else {
+                p1.losses++
+                p2.wins++;
+                console.log("p2wins");
+                gameText.html(s.child("p2/name").val() + "wins!");
+            }
+        } else if (fP1Choice === "paper") {
+            if (fP2Choice === "rock") {
+                p1.wins++;
+                p2.losses++;
+                console.log("p1wins");
+                gameText.html(s.child("p1/name").val() + "wins!");
+            } else {
+                p1.losses++
+                p2.wins++;
+                console.log("p2wins");
+                gameText.html(s.child("p2/name").val() + "wins!");
+            }
+        } else if (fP1Choice === "scissors") {
+            if (fP2Choice === "paper") {
+                p1.wins++;
+                p2.losses++;
+                console.log("p1wins");
+                gameText.html(s.child("p1/name").val() + "wins!");
+            } else {
+                p1.losses++
+                p2.wins++;
+                console.log("p2wins");
+                gameText.html(s.child("p2/name").val() + "wins!");
+            }
         }
         $("#p1-board").html("<i class='fas fa-hand-" + fP1Choice + "'>");
         $("#p2-board").html("<i class='fas fa-hand-" + fP2Choice + "'>");
+        fP1.update(p1);
+        fP2.update(p2);
         nextRound();
         console.log("checkWInnder");
+        d.ref("players/p1/choice").remove();
+        d.ref("players/p2/choice").remove();
     }
 });
 function nextRound() {
     setTimeout(function () {
         $("#p1-board, #p2-board").empty();
-        gameText.text("Choose rock, paper, or scissors.")
-        fP.once("value", function (s) {
-            console.log(s.val());
-            s.child("p1/choice/").val().update("");
-            s.child("p2/choice/").val().update("");
-        });
+        gameText.html("Next Round!<br>Choose rock, paper, or scissors.")
     }, 3000);
 }
 $("#username-submit").on("click", function (event) {
